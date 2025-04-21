91e79180bfbc90d942b667d83970fefafcd87220
Carlos Sac <carlos.sac@outlook.com>
2025-04-20 22:36:32 -0400

    Add basic library tableview

--- Book Vault/ViewController.swift
+++ Book Vault/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Book \(indexPath.row + 1)"
+        return cell
+    }
+    
 
+    @IBOutlet weak var libraryTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        libraryTableView.dataSource = self
+        
     }
 
 
